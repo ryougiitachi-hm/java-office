f215c77fc85877ac0a0ce7e4082768cec74c1426
ryougiitachi <ryougiitachi@hotmail.com>
2023-03-25 18:56:15 +0800

    [office-opencsv-netsf] Practice about net.sf open-csv including reading and writing.

---
+++ office-opencsv-netsf/src/main/java/per/itachi/java/office/opencsv/app/exception/InfraException.java
@@ -0,0 +1,16 @@
+package per.itachi.java.office.opencsv.app.exception;
+
+public class InfraException extends AppException {
+
+    public InfraException() {
+        super();
+    }
+
+    public InfraException(String message) {
+        super(message);
+    }
+
+    public InfraException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
